768a79bf190fad7bfea4d9cf6b5e4f101b7c92cc
wxygeek <wxygeek.abc@gmail.com>
2016-10-16 01:02:55 +0800

    update

--- NodeServer/router.js
+++ NodeServer/router.js
@@ -7,18 +7,5 @@ router.get('/', function*() {
   this.body = 'Everything looks good.';
 });
 
-router.get('/test', function*() {
-  console.log('-----------------this.query--------------------');
-  console.log(this.query);
-  console.log('-----------------this.params--------------------');
-  console.log(this.params);
-  console.log('--------------this.request.body-----------------');
-  console.log(this.request.body);
-  console.log('--------------this.request.files-----------------');
-  console.log(this.request.files);
-
-  this.body = 'success';
-});
-
 exports.router = router;
 exports.serverRouter = router.routes();
